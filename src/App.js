390c749d95fe018867d04f4807de495afdd20903
Muhammad Yousuf <yousuf.hanif550@gmail.com>
2022-09-21 21:36:48 +0500

    Update News & News Item Component
    Create UI of news item using bootstrap & display static data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import NavBar from './Components/NavBar'
 import News from './Components/News'
 
+// API KEY: caa21893e6794c11b48862f2980d9028
 export default class App extends Component {
   render() {
     return (
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
+
   render() {
+    let { title, description, image_url, news_url} = this.props;
     return (
-      <div>NewsItem</div>
-    )
+      <div className='my-3'>
+        <div className='card' style={{ width: "18rem" }}>
+          <img src={image_url} className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>{title}</h5>
+            <p className='card-text'>{description}</p>
+            <a href='/' className='btn btn-sm btn-primary'>
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
