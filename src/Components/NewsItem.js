1c6b172e0f79a8b748adde79817e884dbee3d83c
Muhammad Yousuf <yousuf.hanif550@gmail.com>
2022-09-26 19:24:04 +0500

    Update News Item Component

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,15 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className='card' style={{ width: "18rem" }}>
-          <img
-            src={
-              image_url
-                ? "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"
-                : image_url
-            }
-            className='card-img-top'
-            alt='...'
-          />
+          <img src={image_url} className='card-img-top' alt='...' />
           <div className='card-body'>
             <h5 className='card-title'>{title}...</h5>
             <p className='card-text'>{description}....</p>
