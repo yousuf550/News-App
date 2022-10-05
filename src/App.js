5bb75427c6effa13416d5621eabb0a2aa8c182f5
Muhammad Yousuf <yousuf.hanif550@gmail.com>
2022-10-05 23:19:56 +0500

    Update App
    Hiding API Key by Adding Custom Environment Variables

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 
-// API KEY: caa21893e6794c11b48862f2980d9028
 export default class App extends Component {
 
   pageSize=6
+  apiKey= process.env.REACT_APP_NEWS_API
   
   state = {
     progress: 0
@@ -28,14 +28,14 @@ export default class App extends Component {
           />
           <NavBar />
           <Routes>
-          <Route exact path="/" element={ <News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country='us' category='general' />} />
-          <Route exact path="/business" element={ <News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country='us' category='business' />} />
-          <Route exact path="/entertainment" element={ <News setProgress={this.setProgress}   key="entertainment" pageSize={this.pageSize} country='us' category='entertainment' />} />
-          <Route exact path="/general" element={ <News setProgress={this.setProgress}   key="general2"pageSize={this.pageSize} country='us' category='general' />} />
-          <Route exact path="/health" element={ <News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country='us' category='health' />} />
-          <Route exact path="/science" element={ <News setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country='us' category='science' />} />
-          <Route exact path="/sports" element={ <News setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country='us' category='sports' />} />
-          <Route exact path="/technology" element={ <News setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country='us' category='technology' />} />
+          <Route exact path="/" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={this.pageSize} country='us' category='general' />} />
+          <Route exact path="/business" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}  key="business" pageSize={this.pageSize} country='us' category='business' />} />
+          <Route exact path="/entertainment" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}   key="entertainment" pageSize={this.pageSize} country='us' category='entertainment' />} />
+          <Route exact path="/general" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}   key="general2"pageSize={this.pageSize} country='us' category='general' />} />
+          <Route exact path="/health" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={this.pageSize} country='us' category='health' />} />
+          <Route exact path="/science" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={this.pageSize} country='us' category='science' />} />
+          <Route exact path="/sports" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports" pageSize={this.pageSize} country='us' category='sports' />} />
+          <Route exact path="/technology" element={ <News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology" pageSize={this.pageSize} country='us' category='technology' />} />
           </Routes>
         </BrowserRouter>
         ,
